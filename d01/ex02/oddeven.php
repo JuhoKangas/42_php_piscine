f4e21d318a8bc9264524401ec7828747020a4238
Juho Kangas <jkangas@student.hive.fi>
2022-06-02 13:45:14 +0300

    first commit

---
+++ d01/ex02/oddeven.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+    for (;;){
+        echo "Enter a number: ";
+        $num = fgets(STDIN);
+        if ($num){
+            $num = trim($num);
+            if (!(is_numeric($num))){
+                echo "'$num' is not a number\n";
+                continue;
+            }
+            $num = intval($num);
+            if ($num % 2 == 1) {
+                echo "The number $num is odd";
+            } else {
+                echo "The number $num is even";
+            }
+            echo "\n";
+        }
+        else {
+            echo "\n";
+            exit (0);
+        }
+    }
+?>
