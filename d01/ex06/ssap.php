45438ac9d11c9a0f7fabaece539adb72595f91f9
Juho Kangas <jkangas@student.hive.fi>
2022-06-03 16:18:59 +0300

    Add 06

---
+++ d01/ex06/ssap.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php
+    if ($argc < 2)
+        exit (0);
+    $str = "";
+    for ($i = 1; $i < $argc; $i++){
+        $tmp = preg_replace('/\s+/', ' ', $argv[$i]);
+        $str .= "$tmp ";
+    }
+    $str = trim($str);
+    $arr = explode(' ', $str);
+    sort(($arr));
+    for ($j = 0; $j < count($arr); $j++)
+        echo "$arr[$j]\n";
+?>
