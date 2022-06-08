a248bc1cbe0c3346c8060ff6abd773863dd499f7
Juho Kangas <jkangas@student.hive.fi>
2022-06-08 19:20:25 +0300

    add reverse sort to be accepted

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -1,7 +1,9 @@
 <?php
     function ft_is_sort($tab) {
         $arr = $tab;
+        $rev_arr = $tab;
+        rsort($rev_arr);
         sort($arr);
-        return ($arr === $tab);
+        return ($arr === $tab || $rev_arr === $tab);
     }
 ?>
--- d01/ex08/main.php
+++ d01/ex08/main.php
@@ -3,11 +3,15 @@
     include("ft_is_sort.php");
 
     $tab = array("!/@#;^", "42", "Hello World", "hi", "zZzZzZz");
+    $tab2 = ["a", "b", "c"];
+    $tab3 = ["c", "b", "a"];
+    $tab4 = ["c", "b", "a", "c"];
     $sorted = array("abc", "bca", "cba");
     $tab[] = "What are we doing now ?";
 
-    if (ft_is_sort($tab))
+    if (ft_is_sort($tab3))
         echo "The array is sorted\n";
     else
         echo "The array is not sorted\n";
+    var_dump(ft_is_sort($tab3))
 ?>
