9a09c34648654ac009b4c674376cb05cb52f7ef5
Juho Kangas <jkangas@student.hive.fi>
2022-06-09 15:17:45 +0300

    add ex03

---
+++ d03/ex03/cookie_crisp.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_GET['name'];
+    $value = $_GET['value'];
+    $action = $_GET['action'];
+
+    switch ($action) {
+        case 'set':
+            setcookie($name, $value, time()+3600);
+            break;
+            
+        case 'get':
+            if ($_COOKIE[$name])
+                echo $_COOKIE[$name] . "\n";
+            break;
+            
+        case 'del':
+            unset($_COOKIE[$name]);
+            setcookie($name, $value, time() - 3600);
+            break;
+    }
+?>
