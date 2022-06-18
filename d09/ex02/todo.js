89220816eadf4fabd8dd6ce789f1fe77ab604624
Juho Kangas <jkangas@student.hive.fi>
2022-06-18 17:26:17 +0300

    minor fixes for ex00 and 01, finish ex02

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,35 +1,62 @@
 const button = document.getElementById("new");
+const listDiv = document.getElementById("ft_list");
 const toDoList = new Array();
+let id = 0;
 
-button.addEventListener("click", (e) => {
-	let todo = prompt("What is the todo?");
-	toDoList.unshift(todo);
-	console.log(toDoList);
-});
-
+// Fetch todolist items from cookies
 window.addEventListener("load", (e) => {
 	if (document.cookie) {
-		console.log(document.cookie);
+		let cookieList = JSON.parse(decodeURIComponent(document.cookie));
+		for (i in cookieList) {
+			if (cookieList[i]) {
+				addItem(cookieList[i]);
+				toDoList.push(cookieList[i]);
+			}
+		}
+		console.log(cookieList);
 	}
 });
 
+// Save todo-list items when closing the tab
 window.addEventListener("unload", () => {
 	let listItems = {};
 
+	if (toDoList.length === 0) {
+		document.cookie = "";
+		return;
+	}
 	toDoList.forEach((item, index) => {
 		listItems[index] = item;
 	});
 
 	if (Object.keys(listItems).length !== 0) {
-		document.cookie = JSON.stringify(listItems);
+		document.cookie = encodeURIComponent(JSON.stringify(listItems));
 	}
 });
-// const cookieArr = document.cookie.split("; ");
-// const cookieMap = new Map();
 
-// for (obj of cookieArr) {
-// 	let values = obj.split("=");
-// 	cookieMap.set(values[0], values[1]);
-// }
+button.addEventListener("click", (e) => {
+	let todo = prompt("What is the todo?");
+	if (todo.length > 0) {
+		toDoList.push(todo);
+		addItem(todo);
+	}
+});
 
-// console.log(cookieMap);
+function addItem(item) {
+	const node = document.createElement("div");
+	node.setAttribute("class", "listItem");
+	node.setAttribute("id", id++);
+	node.innerHTML = item;
+	node.addEventListener("click", remove);
+	listDiv.prepend(node);
+}
+
+function remove() {
+	if (confirm("Are you sure?")) {
+		index = toDoList.indexOf(document.getElementById(this.id).innerHTML);
+		console.log(index);
+		toDoList.splice(index, 1);
+		console.log(toDoList);
+		this.remove();
+	}
+}
